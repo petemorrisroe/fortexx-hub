be0457c261ff8f427d42b4b649750d4952218cb6
Pete MorrisRoe <50459086+petemorrisroe@users.noreply.github.com>
2024-10-02 21:08:17 +0100

    241002

--- api/__init__.py
+++ api/__init__.py
@@ -1,43 +1,27 @@
 import azure.functions as func
-import json
+from fasthtml import HTMLTemplateResponse, rt
+from fasthtml.common import *
 
-# def main(req: func.HttpRequest) -> func.HttpResponse:
-    # Handle preflight request (OPTIONS)
-    # if req.method == "OPTIONS":
-    #     headers = {
-    #         'Access-Control-Allow-Origin': 'https://brave-water-0dadd3a10.5.azurestaticapps.net',
-    #         'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-    #         'Access-Control-Allow-Headers': 'Authorization, Content-Type',
-    #         'Access-Control-Allow-Credentials': 'true'
-    #     }
-    #     return func.HttpResponse(status_code=204, headers=headers)
+from cosmos_db import read_articles, write_article
+from htmx import hx
 
-    # # Handle GET request
-    # if req.method == "GET":
-    #     data = {"message": "Hello World"}
-    #     response = func.HttpResponse(
-    #         json.dumps(data),
-    #         mimetype="application/json",
-    #         status_code=200
-    #     )
-    #     # Adding the CORS headers
-    #     response.headers['Access-Control-Allow-Origin'] = 'https://brave-water-0dadd3a10.5.azurestaticapps.net'
-    #     response.headers['Access-Control-Allow-Credentials'] = 'true'
-    #     return response
+app, rt = fast_app()
 
-    # # Handle POST request
-    # if req.method == "POST":
-    #     response = func.HttpResponse(
-    #         "POST request received",
-    #         status_code=200
-    #     )
-    #     # Adding the CORS headers
-    #     response.headers['Access-Control-Allow-Origin'] = 'https://brave-water-0dadd3a10.5.azurestaticapps.net'
-    #     response.headers['Access-Control-Allow-Credentials'] = 'true'
-    #     return response
+@rt("/articles")
+async def articles_list(req: func.HttpRequest) -> func.HttpResponse:
+    articles = read_articles()
+    return HTMLTemplateResponse("article_list.html", {"articles": articles})
 
-    # # Default response for unhandled methods
-    # return func.HttpResponse(
-    #     "Unhandled request method",
-    #     status_code=400
-    # )
+@rt("/articles/add", methods=["GET", "POST"])
+async def add_article(req: func.HttpRequest) -> func.HttpResponse:
+    if req.method == "POST":
+        data = req.form
+        write_article(data)
+        return hx.redirect("/articles")
+    return HTMLTemplateResponse("article_form.html")
+
+@rt("/test")
+async def get():
+    return Titled("FastHTML", P("Let's do this!"))
+
+serve()
