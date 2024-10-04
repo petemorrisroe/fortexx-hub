3aaedfdb5e56b9d76e4e20496e84bf3574aff300
Pete MorrisRoe <50459086+petemorrisroe@users.noreply.github.com>
2024-10-04 15:57:47 +0100

    1557

--- api/cosmos_db.py
+++ api/cosmos_db.py
@@ -3,13 +3,11 @@ import os
 
 COSMOS_URL = os.getenv('COSMOS_URL')
 COSMOS_KEY = os.getenv('COSMOS_KEY')
-
-# Initialize CosmosClient with a dictionary containing the key
-client = CosmosClient(COSMOS_URL, {'masterKey': COSMOS_KEY})
-
 DATABASE_NAME = "fortexx"
 CONTAINER_NAME = "article"
 
+# Initialize CosmosClient with a dictionary containing the key
+client = CosmosClient(COSMOS_URL, COSMOS_KEY)
 database = client.get_database_client(DATABASE_NAME)
 container = database.get_container_client(CONTAINER_NAME)
 
