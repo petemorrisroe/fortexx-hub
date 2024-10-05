473ad7151c6d6d5cbc9a07e6ab88147530f0b695
Pete MorrisRoe <50459086+petemorrisroe@users.noreply.github.com>
2024-10-05 18:01:00 +0100

    1800

--- api/hello_function/__init__.py
+++ api/hello_function/__init__.py
@@ -1,22 +1,7 @@
-import logging
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-            "Please pass a name on the query string or in the request body.",
-            status_code=400
-        )
+    return func.HttpResponse(
+        "Hello, this is a simple function for enabling Application Insights!",
+        status_code=200
+    )
