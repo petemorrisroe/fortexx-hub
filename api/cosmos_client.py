133b320dc9e9e697ad26e4713ef23b448e9b5ec2
Pete MorrisRoe <50459086+petemorrisroe@users.noreply.github.com>
2024-10-06 22:10:20 +0100

    2210

--- api/cosmos_client.py
+++ api/cosmos_client.py
@@ -1,21 +1,21 @@
-from azure.cosmos.aio import CosmosClient
-from azure.cosmos import PartitionKey
 import os
+from azure.cosmos.aio import CosmosClient
+from azure.cosmos.exceptions import CosmosHttpResponseError
 
-URI = os.getenv("COSMOS_URI")
-KEY = os.getenv("COSMOS_KEY")
-
-cosmos_client = CosmosClient(URI, credential=KEY)
-database_name = "my-database"
-container_name = "articles"
+COSMOS_URL = os.getenv("COSMOS_URL")
+COSMOS_KEY = os.getenv("COSMOS_KEY")
+DATABASE_NAME = "fortexxe"
+CONTAINER_NAME = "articles"
 
 async def get_container():
-    database = await cosmos_client.create_database_if_not_exists(id=database_name)
-    container = await database.create_container_if_not_exists(
-        id=container_name, partition_key=PartitionKey(path="/id")
-    )
-    return container
-
+    try:
+        client = CosmosClient(COSMOS_URL, credential=COSMOS_KEY)
+        database = await client.create_database_if_not_exists(DATABASE_NAME)
+        container = await database.create_container_if_not_exists(id=CONTAINER_NAME, partition_key="/id")
+        return container
+    except CosmosHttpResponseError as e:
+        print(f"Error connecting to Cosmos DB: {e.message}")
+        raise
 
 
 
