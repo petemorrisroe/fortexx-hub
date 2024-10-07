a11eb34ef16ad129714b2589f651b59966eb15f9
Pete MorrisRoe <50459086+petemorrisroe@users.noreply.github.com>
2024-10-07 21:47:42 +0100

    2147

--- api/test_function/__init__.py
+++ api/test_function/__init__.py
@@ -1,11 +1,7 @@
 import logging
 import azure.functions as func
 
-async def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    return func.HttpResponse(
-        "I'm alive!",
-        status_code=200
-    )
+    return func.HttpResponse("Test function is working", status_code=200)
 
