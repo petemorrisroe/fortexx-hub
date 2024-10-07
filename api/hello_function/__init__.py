1fc8e536cbc20f057ca20962812012a9c4e7767c
Pete MorrisRoe <50459086+petemorrisroe@users.noreply.github.com>
2024-10-07 13:34:39 +0100

    1334

--- api/hello_function/__init__.py
+++
@@ -1,7 +0,0 @@
-import azure.functions as func
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse(
-        "Hello, this is a simple function for enabling Application Insights!",
-        status_code=200
-    )
--- api/test_function/function_app.py
+++
@@ -1,8 +0,0 @@
-import azure.functions as func
-from fasthtml.common import serve
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("Simple Azure Function Trigger", status_code=200)
-
-if __name__ == "__main__":
-    serve()
