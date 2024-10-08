c3bbbc3e263d74c2a6af7f825d10c26166a15cc1
Pete MorrisRoe <50459086+petemorrisroe@users.noreply.github.com>
2024-10-08 17:50:48 +0100

    1750

--- api/WrapperFunction/__init__.py
+++ api/WrapperFunction/__init__.py
@@ -2,7 +2,10 @@ import fastapi
 
 app = fastapi.FastAPI()
 
+@app.on_event("startup")
+async def startup_event():
+    print("Function App starting up...")
+    
 @app.get("/test")
 async def test_endpoint():
     return {"message": "I'm alive"}
-    
